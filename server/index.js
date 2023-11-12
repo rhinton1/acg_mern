01b118bd5f84b4a5a10cddb6b601c85db6f12350
Ron Hinton <ron_hinton@yahoo.com>
2023-11-12 10:03:37 -0500

    acg_Pilot_0_Add_server_to_database_connection: created the connection between the app server and the app data

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to Data..');
+    })
+    .catch((err) => {
+        console.log('There was a problem connecting to data..')
+    })
+
 app.listen(3111, () => {
-    console.log('ACG is up and running...');
+    console.log('ACG is up and running..');
 });
